cc24162bc07634ea2c728afc66a218dd3a051c91
unknown <you@example.com>
2021-06-28 06:47:36 +0700

    Input and Claculat Number

---
+++ Basic_Input/Input_C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+ int Num1,Num2,CalulatNum;
+    printf("Enter Number1:");
+    scanf("%d",&Num1);
+    printf("Enter Number2:");
+    scanf("%d",&Num2);
+    CalulatNum = Num1 + Num2 + Num2*12;
+    printf("The Result of Calculater Number %d ",CalulatNum);
+    return 0;
+    getch ();
+}
+   
+
