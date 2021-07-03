09ea2daae92595384cdc4e5563bf1f349bbc46cc
unknown <you@example.com>
2021-07-03 09:17:07 +0700

    My Project School

---
+++ School/EP2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	printf("\n");
+	printf("\tExercise 3 \n");
+    printf("\n");
+    printf("+=================================================================================+\n");
+    printf("| ID\tName\t\tGernder\t\tAddress\t\t\tPhone\n");
+    printf("+=================================================================================+\n");
+    printf("| 1\tDara\t\tM\t\tPhnome Penh\t\t018373485\t\t|\n");
+    printf("\n");
+    printf("| 2\tVichet\t\tM\t\tKandal\t\t\t017348384\t\t|\n");
+    printf("\n");
+    printf("| 3\tDalin\t\tF\t\tPrey Veng\t\t012909047\t\t|\n");
+    printf("\n");
+    printf("| 4\tDary\t\tF\t\tPrey Veng\t\t012908070\t\t|\n");
+    printf("\n");
+    printf("| 5\tMessi\t\tM\t\tKompong Cham\t\t012994367\t\t|\n");
+    printf("\n");
+    printf("| 7\tMessi\t\tM\t\tKomepong Cham\t\t012884367\t\t|\n");
+    printf("+.................................................................................+\n");
+    return 0;
+	getch();
+}
